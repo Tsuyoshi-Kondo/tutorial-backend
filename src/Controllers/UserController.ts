93159868fcfb91ae09783c2cc767c9112d6a2762
Tsuyoshi-Kondo <126660316+Tsuyoshi-Kondo@users.noreply.github.com>
2023-04-10 17:56:00 +0900

    リクエストパラメータ起因のエラークラスの追加等

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 
 import { issuanceToken } from "../middleware/JWTManager";
 import {
@@ -6,12 +7,17 @@ import {
   isMatchPassword,
 } from "../middleware/PasswordManager";
 import { getUserByEmail, registerUser } from "../models/UserModel";
+import { RequestError } from "../types/RequestError";
 
 // ユーザーを新規登録する
 export const userRegister = async (req: Request, res: Response) => {
-  const email: string = req.body.email;
-  const password: string = req.body.password;
   try {
+    // バリデーション
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) throw new RequestError();
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+
     // DBにユーザーが存在しているか確認->存在していなければOK
     const user = await getUserByEmail(email);
     if (user !== null) throw new Error();
@@ -21,19 +27,27 @@ export const userRegister = async (req: Request, res: Response) => {
 
     // DBにユーザーを登録
     const newUser = await registerUser(email, hashedPassword);
-    res.status(200).json({ id: newUser.id, email: newUser.email });
-  } catch (error: unknown) {
-    res.status(400).json(null);
+    res.status(200).json({data:{ id: newUser.id, email: newUser.email }, message:"ユーザーの新規登録に成功しました。"});
+  }
+  catch (error: unknown) {
+    if (error instanceof RequestError) {
+      res.status(400).json({data:null, message:error.message});
+      return;
+    }
+    res.status(500).json({data:null, message:"ServerError: サーバーでエラーが発生しました。"});
   }
 };
 
 // ログインする
 export const userLogin = async (req: Request, res: Response) => {
-  const email: string = req.body.email;
-  const password: string = req.body.password;
-  const secretKey: string = process.env.SECRET_KEY??"";
-
   try {
+    // バリデーション
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) throw new RequestError();
+    const email: string = req.body.email;
+    const password: string = req.body.password;
+    const secretKey: string = process.env.SECRET_KEY!;
+
     // ユーザーがDBに存在するか確認->存在していたらOK
     const user = await getUserByEmail(email);
     if (user === null) throw new Error();
@@ -45,8 +59,13 @@ export const userLogin = async (req: Request, res: Response) => {
     // パスワードが合っていたらJWTを発行する
     const token = issuanceToken(secretKey, user.id);
 
-    res.status(200).json({ id: user.id, email: user.email, token: token });
-  } catch (error: unknown) {
-    res.status(400).json(null);
+    res.status(200).json({data:{ id: user.id, email: user.email, token: token }, message:"ログインに成功しました。"});
+  }
+  catch (error: unknown) {
+    if (error instanceof RequestError) {
+      res.status(400).json({data:null, message:error.message});
+      return;
+    }
+    res.status(500).json({data:null, message:"ServerError: サーバーでエラーが発生しました。"});
   }
 };
--- src/middleware/checkJwtHasPermission.ts
+++ src/middleware/checkJwtHasPermission.ts
@@ -1,25 +1,38 @@
 import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
 
-import "dotenv/config";
 import { inspectionToken } from "./JWTManager";
+import { RequestError } from "../types/RequestError";
+import "dotenv/config";
 
 export const checkJwtHasPermission = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const token = req.header("x-auth-token");
-  if (!token) throw new Error();
   try {
-    const secretKey = process.env.SECRET_KEY??"";
+    // バリデーション
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) throw new RequestError();
+
+    const token = req.header("x-auth-token");
+    const secretKey = process.env.SECRET_KEY!;
+
     // リクエストされたJWT文字列と共通鍵を用いてJWTの検証と読み取りを行う
-    const decoded = await inspectionToken(secretKey, token);
+    const decoded = await inspectionToken(secretKey, token!);
+
+    // リクエストされたユーザーIDとJWTのユーザーIDが一致しているか確認
     if (parseInt(decoded.id) !== parseInt(req.params.userId)) {
       throw new Error();
     }
-  } catch (error: unknown) {
-    res.status(400).json(null);
-    return
+  }
+  catch (error) {
+    if (error instanceof RequestError) {
+      res.status(400).json({data:null, message:error.message});
+      return;
+    }
+    res.status(500).json({data:null, message:"ServerError: サーバーでエラーが発生しました。"});
+    return;
   }
   next();
 };
