347e13d19bf264b15e165db1d1c6c4aa4f17fc6d
Tsuyoshi-Kondo <126660316+Tsuyoshi-Kondo@users.noreply.github.com>
2023-04-10 13:15:02 +0900

    パスワードのハッシュ化関連コメントの修正

--- src/middleware/PasswordManager.ts
+++ src/middleware/PasswordManager.ts
@@ -1,12 +1,14 @@
 import bcrypt from "bcrypt";
 
-// パスワードの暗号化
+// パスワードのハッシュ化
 export const getHashedPassword = async (password: string) => {
+  // bcrypt.hash(ハッシュ化したい平文パスワード, ハッシュ化の回数)
   const hashedPassword: string = await bcrypt.hash(password, 10);
   return hashedPassword;
 };
 
-// パスワードを複号して照合する
+// 平文パスワードとDBに登録されているハッシュ値を受け取る
+// 平文パスワードのハッシュ値とDBに登録されているハッシュ値を照合する
 export const isMatchPassword = async (
   password: string,
   passwordFromDB: string
